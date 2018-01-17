97469a23c17ade00e333d9972b42fb30a89804db
EvaFro <frolli.erin@gmail.com>
2018-01-16 20:51:43 -0800

    Started this sprint - made style changes

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,31 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+
+  // Obj: {}
+  // Array: []
+  // string: ""
+  // values: values
+
+  // pairs: value:value 
+
+  // spaces: " " 
+  while (json[0] === ' ') { json = json.substr(1); }
+  while (json[json.length - 1] === ' ') { 
+    json = json.substr(0, json.length - 1);
+  }
+  // json.substr(1)
+  parseJSON(json);
+  // for an object
+  if (json[0] === '{' && json[json.length - 1] === '}') {
+    var result = {};
+  }
+  
+  // 4 an array
+  if (json[0] === '[' && json[json.length - 1] === ']') {
+
+  }
+    
 };
+
+//'{"genre":"comedy","movies":[{"name":"Elf","score":8.7},{"name":"Spaceballs","score" : 7.7}]}'
